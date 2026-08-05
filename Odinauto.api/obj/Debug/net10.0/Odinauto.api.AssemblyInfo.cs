f70d390eb053ceb77017f7c75439e1a1ed4def2d
Hamza Simsek <hamzas4011@hotmail.com>
2026-08-05 23:03:19 +0200

    connecting database

--- Odinauto.api/obj/Debug/net10.0/Odinauto.api.AssemblyInfo.cs
+++ Odinauto.api/obj/Debug/net10.0/Odinauto.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c76cbd6f3e08b8aacbccb939bd2ab3975a8160")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
