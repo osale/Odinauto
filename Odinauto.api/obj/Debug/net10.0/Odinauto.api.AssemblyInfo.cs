f432df896c3397b1b5974e41e18addfce396dcc9
Hamza Simsek <hamzas4011@hotmail.com>
2026-08-06 09:29:33 +0200

    project created

--- Odinauto.api/obj/Debug/net10.0/Odinauto.api.AssemblyInfo.cs
+++ Odinauto.api/obj/Debug/net10.0/Odinauto.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96daec6e813174d4747eef7406f3bc5266c313e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8224d442da5d2d0e079068603bf5daf28a38552")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
