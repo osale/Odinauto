3b157bdb7da0eda2f8f01ec5685a30da1518b261
Hamza Simsek <hamzas4011@hotmail.com>
2026-08-17 08:40:03 +0200

    update classes

--- Odinauto.api/obj/Debug/net10.0/Odinauto.api.AssemblyInfo.cs
+++ Odinauto.api/obj/Debug/net10.0/Odinauto.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a8ce8f6d9c5aba224276c16febe0534ff14718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f32d950dee6f3e65fe195954103cfb97dfefaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odinauto.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
