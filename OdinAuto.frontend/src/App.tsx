21416f9fcb509826ecf50f90b2874eafa3ffd11f
Shabnam iftikhar <100565090+shab30@users.noreply.github.com>
2026-08-18 13:07:10 +0200

    Add React TypeScript frontend

---
+++ OdinAuto.frontend/src/App.tsx
@@ -0,0 +1,19 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const companyName : string = "Odin Auto";
+  
+  return (
+    <div>
+      <h1>Welcome to{companyName}</h1>
+      <h3>Knowledge Assistant</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default App
